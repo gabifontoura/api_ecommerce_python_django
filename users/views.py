a83d1b9d287d8767a3abf4767aa0ff0865ffabe5
gabriel <gmachadoporto@gmail.com>
2023-05-03 15:47:14 -0300

    feat: add permissions

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from users.models import User
 from users.serializers import UserSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.shortcuts import get_object_or_404
-from users.permissions import IsAdminOrReadOnly, HasAdminPermission
+from users.permissions import IsOwnerOrAdminPermission
 from rest_framework import generics
 from rest_framework.permissions import (
     IsAdminUser,
@@ -13,7 +13,13 @@ from rest_framework.permissions import (
 )
 
 
-class UserView(generics.ListCreateAPIView):
+class UserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserAllView(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -21,7 +27,7 @@ class UserView(generics.ListCreateAPIView):
 
 class UserViewDetail(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [HasAdminPermission]
+    permission_classes = [IsOwnerOrAdminPermission]
     queryset = User.objects.all()
     serializer_class = UserSerializer
     url_params_name = "user_id"
