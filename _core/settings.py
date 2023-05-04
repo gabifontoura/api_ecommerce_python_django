bed1b1af8508ca02b5d73aecb72f86337aca0013
Guileano Gadea de Mello <110124549+Guiles10@users.noreply.github.com>
2023-05-04 09:04:15 -0300

    feat: swegger

--- _core/settings.py
+++ _core/settings.py
@@ -149,7 +149,7 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 2,
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
 }
 
 # Internationalization
